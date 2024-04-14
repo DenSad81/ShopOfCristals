1485b259f5cae293a0f26e1a9c75c2d7ed97ca19
DenSad81 <tagil81@mail.ru>
2024-04-14 11:02:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        int gold;
+        int cristalPrise = 52;
+        int crystalsToBuy;
+
+        Console.Write("Сколько золота вы хотите? ");
+        gold = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Сколько кристаллов вы хотите купить? ");
+        crystalsToBuy = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+        gold -= crystalsToBuy * cristalPrise;
+        Console.WriteLine($"Количество кристаллов {crystalsToBuy}, остаток золота {gold}");
+    }
+}
